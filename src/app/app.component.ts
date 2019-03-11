170706816e3b0cd0a79b81129fafd49727e24fed
ninohero <ninohero@muzocalo.com>
2019-03-11 15:16:11 -0400

    new things added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  componentTitle = 'this is an impoprted app component title';
+
+  clickHandler() {
+    console.log("click");
+  }
 }
